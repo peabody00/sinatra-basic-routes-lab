13fb274bbd877cf88286ab1c9bc72ff6b01a27d9
Charlie <peabody00@gmail.com>
2021-02-08 12:42:41 -0600

    commit tree 9eceb54fdfef2153712c52b03946b1e79c3aee08

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Charlie"
+    end 
+
+    get '/hometown' do
+        "My hometown is East Moline"
+    end 
+
+    get '/favorite-song' do
+        "My favorite song is Something"
+    end 
 end
